89498f6e705a52d8ae0da6ad7646fe2e7d662de4
Abdullah Ali <abdullahali.and1@gmail.com>
2023-05-03 00:18:03 +0700

    Menambahkan Favicon pada web dan database

--- resources/views/admin/akun-admin.blade.php
+++ resources/views/admin/akun-admin.blade.php
@@ -6,7 +6,6 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link href="@vite(['resources/css/app.css', 'resources/js/app.js'])">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
