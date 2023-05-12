85b8718b7a54a010f2e40c16ed714b5f64ce0f81
Abdullah Ali <abdullahali.and1@gmail.com>
2023-05-12 15:37:28 +0700

    Update Versi Tailwind dan merubah letak css untuk tampilan 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
     <title> 404 - Not Found </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/errors.css') }}" rel="stylesheet">
 </head>
 
 <div class="mt-80 text-center">
